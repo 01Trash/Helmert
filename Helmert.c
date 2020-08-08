d2da9a3ee7dad9ab36d050d4534b04f6f3ea8e4d
Erdem <57351007+ErdemCmd@users.noreply.github.com>
2020-08-08 19:36:32 +0300

    Update Helmert.c

--- Helmert.c
+++ Helmert.c
@@ -8,6 +8,7 @@ int main()
 
 	double ED50yuk1, ED50sag1, ED50yuk2, ED50sag2, ED50yuk3, ED50sag3, ED50yuk4, ED50sag4, ED50yuk5, ED50sag5, ED50yuk6, ED50sag6, ED50yuk7, ED50sag7, ED50yuk8, ED50sag8, ED50yuk9, ED50sag9, ED50yuk10, ED50sag10;
 	double ITRFyuk1, ITRFsag1, ITRFyuk2, ITRFsag2, ITRFyuk3, ITRFsag3, ITRFyuk4, ITRFsag4, ITRFyuk5, ITRFsag5, ITRFyuk6, ITRFsag6, ITRFyuk7, ITRFsag7, ITRFyuk8, ITRFsag8, ITRFyuk9, ITRFsag9, ITRFyuk10, ITRFsag10;
+	double a11, a12, a13, a14, a21, a22, a23, a24, a31, a32, a33, a34, a41, a42, a43, a44, a51, a52, a53, a54, a61, a62, a63, a64, a71, a72, a73, a74, a81, a82, a83, a84, a91, a92, a93, a94, a101, a102, a103, a104;
 
 		/// Bilinenleri isteyelim.
 	/* ED50 projeksiyon koordinatları; ED50yuk1, ED50sag1, ED50yuk2, ED50sag2, ED50yuk3, ED50sag3, ED50yuk4, ED50sag4, ED50yuk5, ED50sag5, ED50yuk6, ED50sag6, ED50yuk7, ED50sag7, ED50yuk8, ED50sag8, ED50yuk9, ED50sag9, ED50yuk10, ED50sag10; */
@@ -118,19 +119,66 @@ int main()
 	a13 = ED50yuk1;
 	a14 = -ED50sag1;
 	a21 =………… */
-	
-	/* a11 = 1;
+	// a11, a12, a13, a14;
+	a11 = 1;
 	a12 = 0;
 	a13 = ED50yuk1;
 	a14 = -ED50sag1;
+	// a21, a22, a23, a24;
 	a21 = 1;
 	a22 = 0;
 	a23 = ED50yuk2;
 	a24 = -ED50sag2;
+	// a31, a32, a33, a34;
 	a31 = 1;
 	a32 = 0;
 	a33 = ED50yuk3;
-	a34 = -ED50sag3; */
+	a34 = -ED50sag3;
+	// a41, a42, a43, a44;
+	a41 = 1;
+	a42 = 0;
+	a43 = ED50yuk4;
+	a44 = -ED50sag4;
+	// a51, a52, a53, a54;
+	a51 = 1;
+	a52 = 0;
+	a53 = ED50yuk5;
+	a54 = -ED50sag5;
+	// a61, a62, a63, a64;
+	a61 = 1;
+	a62 = 0;
+	a63 = ED50yuk6;
+	a64 = -ED50sag6;
+	// a71, a72, a73, a74;
+	a71 = 1;
+	a72 = 0;
+	a73 = ED50yuk7;
+	a74 = -ED50sag7;
+	// a81, a82, a83, a84;
+	a81 = 1;
+	a82 = 0;
+	a83 = ED50yuk8;
+	a84 = -ED50sag8;
+	// a91, a92, a93, a94;
+	a91 = 1;
+	a92 = 0;
+	a93 = ED50yuk9;
+	a94 = -ED50sag9;
+	// a101, a102, a103, a104;
+	a101 = 1;
+	a102 = 0;
+	a103 = ED50yuk10;
+	a104 = -ED50sag10;
+	// a111, a112, a113, a114;
+	// a121, a122, a123, a124;
+	// a131, a132, a133, a134;
+	// a141, a142, a143, a144;
+	// a151, a152, a153, a154;
+	// a161, a162, a163, a164;
+	// a171, a172, a173, a174;
+	// a181, a182, a183, a184;
+	// a191, a192, a193, a194;
+	// a201, a202, a203, a204;
 
 	/* l matrisi;
 	> l11, l21, l31, l41, l51, l61, l71, l81, l91, l101, l111, l121, l131, l141, l151, l161, l171, l181, l191, l201
